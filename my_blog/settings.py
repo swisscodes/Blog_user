a6af52abbb8dd46f16e91f9d4ad2b3cfd856eaeb
Swissbobo <swissbobomail.com>
2021-04-30 16:48:30 +0300

    changed my procfile mistake

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))] # new
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # new
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')# new
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
