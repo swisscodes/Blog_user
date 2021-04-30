12b57f716e8c7cabe456146b5fa54811d7b748db
Swissbobo <swissbobomail.com>
2021-04-30 20:23:16 +0300

    ok1

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from environs import Env
-import dj_database_url
 env = Env() # new
 env.read_env() # new
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -97,16 +96,18 @@ WSGI_APPLICATION = 'my_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': 'swiss_blog',
-        #'USER': env.str('DBUSER'),
-        #'PASSWORD': env.str('DBPASSWORD'),
-        #}
-#}
-DATABASES = {
-    "default": dj_database_url.config(default='postgres://kkydurgwxboecy:ccf534e1f229d1606c376b26b9243a6de4d4799938d81efa6b2182b73e1726a0@ec2-54-163-254-204.compute-1.amazonaws.com:5432/da2e4i650vfk7l')
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'swiss_blog',
+            'USER': env.str('DBUSER'),
+            'PASSWORD': env.str('DBPASSWORD'),
+            }
+    }
+else:
+    DATABASES = {
+        "default": env.dj_db_url("DATABASE_URL")
 }
 
 
