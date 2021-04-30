822c91382612f52a0248bd69a1057a8348e75d2d
Swissbobo <swissbobomail.com>
2021-04-30 22:57:34 +0300

    free

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from environs import Env
-import dj_database_url
 env = Env() # new
 env.read_env() # new
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -109,8 +108,7 @@ if DEBUG:
 else:
     #PRODUCTION DATABSE SETTINGS
     DATABASES = {
-        'ENGINE': 'django.db.backends.postgresql',
-        'default': dj_database_url.config(default='postgres://rtuwcebhghrnea:bffa1a758490f99e8b6d79ae15f7f012676f9e93e00e966282611b4e39e7c229@ec2-52-21-252-142.compute-1.amazonaws.com:5432/d9majp8rkc4jjp')
+        "default": env.dj_db_url("DATABASE_URL")
 }
 
 
