18ae98875cdc7ea84984169152a690f619439484
Swissbobo <swissbobomail.com>
2021-05-03 19:50:08 +0300

    testing jquery

--- blog/static/js/jquery.js
+++ blog/static/js/jquery.js
@@ -1,5 +1,8 @@
-$ ( document ).ready ( function ( ) {
-
+$( function () {
     // You have to input your actions in this part
+    $(".login").on("click", function(){
+        return alert("wow");
+    });
+        
     
-} ) ;
+} );
